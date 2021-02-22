844e4eddaa8c090c470457125547c1c4e496fad4
IMayCode <imaycode@gmail.com>
2021-02-22 00:46:41 -0600

    create menu button and add javascript to handle state changes on the dom

---
+++ dist/js/main.js
@@ -0,0 +1,31 @@
+// Select elements from the DOM
+const menuBtn = document.querySelector('.btn-menu');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+// Set initial state of menu to hidden
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+const toggleMenu = () => {
+  if (!showMenu) {
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    navItems.forEach((item) => item.classList.add('show'));
+    // Set menu state
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    navItems.forEach((item) => item.classList.remove('show'));
+    // Set menu state
+    showMenu = false;
+  }
+};
